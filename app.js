bdce9d3724ad807664c3d9fc12c227ac8a03b65d
蒼時弦也 <elct9620@frost.tw>
2012-08-19 01:26:01 +0800

    init

---
+++ app.js
@@ -0,0 +1,66 @@
+/*jslint node: true,nomen: false*/
+/**
+ * Module dependencies.
+ */
+
+"use strict";
+
+var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path');
+
+var app = express();
+
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function () {
+    app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/comet.js', function (req, res) {
+    res.redirect('/socket.io/socket.io.js');
+});
+
+var server = http.createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+});
+
+// assuming io is the Socket.IO server object
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
+
+//Socket IO
+io.sockets.on('connection', function (socket) {
+    //Request Join
+    socket.emit('join');
+    socket.on('join', function (data) {
+        socket.set('nickname', data.nickname, function () {
+            socket.emit('ready');
+            socket.broadcast.emit('chat', {nickname: 'System', message: data.nickname + ' join caht.'});
+        });
+    });
+
+    socket.on('chat', function (data) {
+        socket.get('nickname', function (err, nickname) {
+            socket.broadcast.emit('chat', {nickname: nickname, message: data.message});
+            socket.emit('chat', {nickname: nickname, message: data.message});
+        });
+    });
+});
---
+++ public/javascripts/jquery.chater.js
@@ -0,0 +1,77 @@
+/*jslint browser: true */
+/*global jQuery,io*/
+
+(function ($) {
+    "use strict";
+
+    $.fn.chater = function () {
+
+        var self = this,
+            socket = io.connect('');
+
+        function enableChatarea() {
+            var chatBlock = document.createElement('div'),
+                chats = document.createElement('div'),
+                typeArea = document.createElement('div');
+
+            $(typeArea).html('<form><input id="message" name="message" placeholder="Message" type="text" /><button class="button secondary" type="submit">Say</button>');
+            $(self).append($(chatBlock).append(chats)).append(typeArea);
+
+            $(chatBlock).height(($(window).height() / 2));
+            $(chatBlock).css({
+                'overflow': 'scroll',
+                'margin-bottom': '1em'
+            });
+
+            $(window).resize(function () {
+                $(chatBlock).height($(window).height() / 2);
+            });
+
+            $(typeArea).children('form').submit(function (e) {
+                var message = $(this).children('#message').val();
+                socket.emit('chat', {message: message});
+                $(this).children('#message').val('');
+                e.preventDefault();
+            });
+
+            socket.on('chat', function (data) {
+                $(chats).append('<p><span class="label radius secondary">' + data.nickname + '</span> ' + data.message + '</p>');
+                $(chatBlock).animate({'scrollTop': $(chats).height()}, 'slow');
+            });
+        }
+
+        socket.on('join', function (data) {
+            console.log('Receive Join Event');
+            var nickname = 'Guest',
+                revealWindow = document.createElement('div');
+
+            $('body').append(revealWindow);
+            $(revealWindow).addClass('reveal-modal').html('<div class="alert-box alert" id="alert"></div><h2>Join Chatroom</h2><p>Please type your nickname</p><form><input id="nickname" name="nickname" placeholder="Guest" type="text" /><button class="button secondary" type="submit">Join!</button></form><a class="close-reveal-modal">&#215;</a>');
+            $(revealWindow).children('#alert').hide();
+
+            $(revealWindow).reveal();
+
+            $(revealWindow).children('form').submit(function (e) {
+                $(revealWindow).children('#alert').hide();
+                nickname = $(this).children('#nickname').val();
+
+                if (nickname === null || nickname.length <= 0) {
+                    $(revealWindow).children('#alert').html('You must type your nickname').fadeIn();
+                } else {
+                    socket.emit('join', {nickname: nickname});
+                    socket.on('ready', function () {
+                        $(revealWindow).trigger('reveal:close');
+                        enableChatarea();
+                    });
+                }
+
+                e.preventDefault();
+            });
+
+        });
+
+        return this;
+    };
+
+    $(document).foundationAlerts();
+}(jQuery));
